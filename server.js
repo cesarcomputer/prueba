dd15b5a216fd148b236004cd5ddeca62152aa34f
cesar <cesar_oyarzo_1983@hotmail.com>
2025-11-28 00:19:37 -0300

    feat: pass Pug variables and configure PORT via .env

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+
+// Set Pug as the view engine
+app.set('view engine', 'pug');
+// Set the views directory to ./views/pug
+app.set('views', './views/pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,6 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
+  // Render the Pug template for the index page
+  res.render('index', { title: 'Hello', message: 'Please log in' });
 
 });
 
